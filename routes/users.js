ab2748e036da70ed1dd8dd507a49ed65f5c350b6
stenehr <sten.ehrberg@gmail.com>
2021-09-12 23:03:33 +0300

    salt and hash passwords

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const { User, validateUser } = require('../models/user');
 const { successResult, failureResult } = require('../utils');
@@ -11,19 +13,23 @@ router.post('/', async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    let user = User.findOne({ email });
+    let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json(failureResult('User already registered'));
     }
 
+    const salt = await bcrypt.genSalt(10);
+
     user = new User({
       name,
       email,
-      password,
+      password: await bcrypt.hash(password, salt),
     });
 
     await user.save();
-    return res.status(201).json(successResult(user));
+    return res
+      .status(201)
+      .json(successResult(_.pick(user, ['_id', 'name', 'email'])));
   } catch (ex) {
     return res.status(500).json(failureResult(ex.message));
   }
